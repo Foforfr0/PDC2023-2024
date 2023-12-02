8a60181a200f97f77db28a14e25fb7ab40b9e8a4
x3776x <abrahamvquinto@gmail.com>
2023-12-02 13:47:21 -0600

    nada nuevo.

--- src/eminus5/databaseManagment/model/DAO/DefectoDAO.java
+++ src/eminus5/databaseManagment/model/DAO/DefectoDAO.java
@@ -5,7 +5,25 @@
 
 package eminus5.databaseManagment.model.DAO;
 
+import eminus5.databaseManagment.model.OpenConnectionDB;
+import eminus5.databaseManagment.model.ResultOperation;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 
 public class DefectoDAO {
-
+    public static ResultOperation getDefectos(int idUser) throws SQLException{
+        Connection connectionDB = OpenConnectionDB.getConnection();
+        ResultOperation resultOperation = null;
+        
+        if (connectionDB != null) {
+            String sqlQuery = "SELECT ";
+            PreparedStatement prepareQuery = connectionDB.prepareStatement(sqlQuery);
+            prepareQuery.setInt(1, idUser);
+            ResultSet resultQuery = prepareQuery.executeQuery();
+        }
+        return resultOperation;
+    }
 }
